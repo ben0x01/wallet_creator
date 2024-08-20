d25f27f55e055244f816590c096f3eb1371afc41
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-08-20 18:00:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
-from runner import solana_wallet_create, bitcoin_wallet_create
+from runner import solana_wallet_create, bitcoin_wallet_create, eth_wallet_create, ton_wallet_create
 
 
 def main_menu():
     print("Main Menu")
     print("1. Run generate solana wallets")
     print("2. Run generate bitcoin wallets ")
-    print("3. Exit")
+    print("3. Run generate eth wallets")
+    print("4. Run generate ton wallets")
+    print("5. Exit")
 
     choice = input("Choose an option: ").strip()
 
@@ -15,6 +17,10 @@ def main_menu():
     elif choice == '2':
         asyncio.run(bitcoin_wallet_create())
     elif choice == '3':
+        asyncio.run(eth_wallet_create())
+    elif choice == '4':
+        asyncio.run(ton_wallet_create())
+    elif choice == '5':
         print("Exiting...")
         return
     else:
