d80c1dfc17908b094523f24667c7ce4e41c9777d
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-16 19:12:16 +0500

    Update eth.py

--- modules/eth.py
+++ modules/eth.py
@@ -18,12 +18,17 @@ class AsyncEthWalletGenerator:
         return address, private_key
 
     async def write_wallets_to_txt(self, filename):
-        async with async_open(filename, 'w') as afp:
-            tasks = [self.generate_wallet() for _ in range(self.wallet_amounts)]
-            wallets = await asyncio.gather(*tasks)
+        tasks = [self.generate_wallet() for _ in range(self.wallet_amounts)]
+        wallets = await asyncio.gather(*tasks)
 
-            for wallet in wallets:
-                await afp.write(f"WALLET: {wallet[0]} - PRIVATE KEY: {wallet[1]}\n")
+        data = {
+            "WALLET": [wallet[0] for wallet in wallets],
+            "PRIVATE KEY": [wallet[1] for wallet in wallets],
+        }
+
+        df = pd.DataFrame(data)
+
+        df.to_excel(f"{filename}.xlsx", index=False)
 
     async def run(self):
         await self.write_wallets_to_txt(self.filename)
