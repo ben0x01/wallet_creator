f52f1a5de29730e229747d86fea660bb33295b9d
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-16 20:45:30 +0500

    Update ton.py

--- modules/ton.py
+++ modules/ton.py
@@ -1,40 +1,49 @@
 import asyncio
-
+import pandas as pd
 from aiofile import async_open
 from tonsdk.contract.wallet import WalletVersionEnum, Wallets
 from tonsdk.crypto import mnemonic_new
 from tonsdk.utils import bytes_to_b64str
-
 
 class AsyncTonWalletGenerator:
     def __init__(self, wallet_amounts, filename):
         self.wallet_amounts = wallet_amounts
         self.filename = filename
 
-    @staticmethod
-    async def generate_wallet():
+    async def generate_wallet(self):
         wallet_workchain = 0
         wallet_version = WalletVersionEnum.v3r2
         wallet_mnemonics = mnemonic_new()
 
         _mnemonics, _pub_k, _priv_k, wallet = Wallets.from_mnemonics(
             wallet_mnemonics, wallet_version, wallet_workchain)
+
         query = wallet.create_init_external_message()
         bytes_to_b64str(query["message"].to_boc(False))
 
-        return wallet.address.to_string(True, True, True), wallet_mnemonics
+        user_friendly_address = wallet.address.to_string(is_user_friendly=True)
+        raw_address = wallet.address.to_string(is_user_friendly=False)
 
-    async def write_wallets_to_txt(self, filename):
-        async with async_open(filename, 'w') as afp:
+        user_friendly_address_safe = user_friendly_address.replace('+', '-').replace('/', '_')
+        raw_address_safe = raw_address.replace('+', '-').replace('/', '_')
 
-            tasks = []
-            for _ in range(self.wallet_amounts):
-                tasks.append(self.generate_wallet())
+        return user_friendly_address_safe, raw_address_safe, wallet_mnemonics
 
-            wallets = await asyncio.gather(*tasks)
+    async def write_wallets_to_excel(self, filename):
+        tasks = []
+        for _ in range(self.wallet_amounts):
+            tasks.append(self.generate_wallet())
 
-            for wallet in wallets:
-                await afp.write(f"WALLET: {wallet[0]} - PRIVATE KEY: {wallet[1]}\n")
+        wallets = await asyncio.gather(*tasks)
+
+        data = {
+            "WALLET": [wallet[0] for wallet in wallets],
+            "MNEMONIC": [' '.join(wallet[2]) for wallet in wallets],
+        }
+
+        df = pd.DataFrame(data)
+
+        df.to_excel(f"{filename}.xlsx", index=False)
 
     async def run(self):
-        await self.write_wallets_to_txt(self.filename)
+        await self.write_wallets_to_excel(self.filename)
